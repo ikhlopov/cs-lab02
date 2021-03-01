ca8a3790d3eaacd26000db20aae54abfba2062a4
ikhlopov <78871193+ikhlopov@users.noreply.github.com>
2021-03-01 20:53:53 +0300

    code: добавлен поиск максимума

--- main.cpp
+++ main.cpp
@@ -10,5 +10,9 @@ int main()
     cout << "A + B = " << a + b << '\n'
         << "A - B = " << a - b << '\n'
         << "A * B = " << a * b << '\n'
-        << "A / B = " << a / b << '\n' ;
+        << "A / B = " << a / b << '\n';
+
+    cout << "Max(A,B) = ";
+    if (a>b) cout << a << '\n';
+    else cout << b << '\n';
 }
